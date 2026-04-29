f3420ca13fa96f2360a7b4a9761c9859c11442e3
Seth Hartman <104603075+SethHartman13@users.noreply.github.com>
2026-04-29 12:52:11 -0600

    Update generalScripts.js

--- Data/website/scripts/generalScripts.js
+++ Data/website/scripts/generalScripts.js
@@ -27,3 +27,15 @@ document.addEventListener("DOMContentLoaded", function() {
         input.setAttribute("autocomplete", "off");
     });
 });
+
+// Source - https://stackoverflow.com/a/65502583
+// Posted by Wenfang Du, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-29, License - CC BY-SA 4.0
+
+window.addEventListener('beforeunload', evt => {
+  // Recommended
+  evt.preventDefault()
+  // Included for legacy support, e.g. Chrome/Edge < 119
+  evt.returnValue = true
+})
+
